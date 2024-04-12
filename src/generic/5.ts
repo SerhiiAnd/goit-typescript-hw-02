a4e447e6110e4f941c344b87fa71a91eb1e06950
SerhiiAnd <136363351+SerhiiAnd@users.noreply.github.com>
2024-04-12 10:45:10 +0200

    12.04-save1

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,11 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<K, V> {
+  key: K;
+  value: V;
 }
 
+const obj1: KeyValuePair<string, number> = { key: "number", value: 111 };
+const obj2: KeyValuePair<number, number[]> = { key: 123, value: [1, 3, 5] };
 export {};
